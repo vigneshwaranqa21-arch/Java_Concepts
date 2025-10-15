cc7e3b0a2cf7f2fea4979d01b6216ced35d57076
Vigneshsubramaniyan <vigneshsubramaniyan97@gmail.com>
2025-10-15 22:40:10 +0530

    First Commit

---
+++ Basics.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Basics {
+		
+	public static void main(String [] args) {
+	
+		System.out.println("First Program");
+		
+		int a = 3 , b = 21;
+		
+		int c = a+b;
+		
+		System.out.println(c);
+		System.out.println(5+c);
+		
+
+	}
+	
+
+}
